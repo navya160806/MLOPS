a0286774f7bb3271440126e3d3bc1cdc40902bcb
navya singh <iamnavyasingh2006@gmail.com>
2026-03-09 23:06:28 +0530

    .

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = "secret123"
+
+# INDEX PAGE
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
