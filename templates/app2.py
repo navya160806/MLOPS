7e141480f356a0ee33ce4514a42bcffe0af45781
navya singh <iamnavyasingh2006@gmail.com>
2026-03-20 23:33:41 +0530

    .

--- templates/app2.py
+++ templates/app2.py
@@ -75,7 +75,6 @@ def register():
     return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
-def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
